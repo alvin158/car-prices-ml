51171c38a9321908835565a103de068ce9089659
yang696 <yangyue353@hotmail.com>
2021-08-18 10:48:28 +0800

    Change to random forest ML

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
